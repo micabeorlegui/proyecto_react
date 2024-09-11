29796ac9b194f95b8f0b9194c7554b61dfce6744
jazbang <jbang@udesa.edu.ar>
2024-09-11 19:08:00 -0300

    arranqué lo del formulario de Ver Más

---
+++ proyecto_react/src/components/FormularioVerMas/FormularioVerMas.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class FormularioVerMas extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            valor: ''
+        };
+    }
+
+    evitarSubmit(event){
+        event.preventDefault();
+    }
+
+    render() {
+        return(
+            <form onSubmit={(event)=>this.evitarSubmit(event)}>
+                <label>Filtrar:</label>
+                <input type='text' value={this.state.valor} onChange={''}/>
+                <input type='submit' value=''/>
+            </form>
+        );
+    }
+
+}
+
+export default FormularioVerMas;
---
+++ proyecto_react/src/components/pages/VerTodas.js
@@ -0,0 +1,11 @@
+import FormularioVerMas from "../FormularioVerMas/FormularioVerMas";
+
+const VerTodas= () => {
+    return(
+        <>
+           
+        </>
+    )
+}
+
+export default VerTodas;
