16476db5a62f767bd66d27a1c6201309fb2d8667
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-09-11 14:19:30 -0300

    arranque y termine el footer

--- proyecto_react/src/App.jsx
+++ proyecto_react/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header/>
+      <h1>Hola</h1>
+      <Footer/>
     </>
   );
 }
---
+++ proyecto_react/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+        <p>Proyecto realizado por Micaela Beorlegui, Tomas Castro y Jazmin Bang</p>
+        </footer>
+    )
+}
+
+export default Footer;
