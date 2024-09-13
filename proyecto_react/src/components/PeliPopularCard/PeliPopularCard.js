eb690eec96a9e21c0c519d3add7f09f6cbb00ed4
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-09-13 19:21:50 -0300

    hice el primer fetch con la api de peliculas populares

--- proyecto_react/src/components/PeliPopularCard/PeliPopularCard.js
+++ proyecto_react/src/components/PeliPopularCard/PeliPopularCard.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Component } from "react";
 import "./PeliPopularCard.css"
 
@@ -9,22 +10,22 @@ class PeliPopularCard extends Component{
         }
     }
     render(){
+        const {title,overview, poster_path}= this.props.pelicula
         return(
             <>
                 <article className='character-card'>
-                        <img src={image} alt="" />{/* arreglar*/}
-                        <h2> Nombre</h2> {/* Nombre */}{/* arreglar*/}
-                        <p>Descripcion entre llaves </p> {/* descripcion - hacerlo link*/}
+                        <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={title} />
+                        <h2> {title}</h2> {/* Nombre */}
                         <p> Ir a detalle - hacer link</p>{/* arreglar*/}
                         <p> Agregar o eliminar de favoritos- hacer link</p>{/* arreglar*/}
-                        <Link to={`/personajes/id/${id}`}>Ver mas</Link> {/* arreglar*/}
+                        {/* <Link to={`/personajes/id/${id}`}>Ver mas</Link>  arreglar*/}
+                        <p>{overview} </p> {/* descripcion - hacerlo link*/}
                         <p className='more' onClick={()=> this.verMas()}>{this.state.verMas ? "ver menos" : 'ver mas'}</p> {/* arreglar*/}
                         {this.state.verMas &&(                          
                             <section className='extra'>                 
                             <p>Origen: {origin.name} </p>               
                             </section>                              
                         )}
-                        {/* arreglar*/}
                         <p className='delete'>Borrar</p>
                     </article>
             </>
--- proyecto_react/src/components/PeliculasPopulares/PeliculasPopulares.js
+++ proyecto_react/src/components/PeliculasPopulares/PeliculasPopulares.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Component } from "react";
+import PeliPopularCard from "../PeliPopularCard/PeliPopularCard";
+import './PeliculasPopulares.css'
 
 class PeliculasPopulares extends Component{
     constructor(){
@@ -8,10 +10,22 @@ class PeliculasPopulares extends Component{
             arrayPelicula:[]
         }
     }
+    componentDidMount( ) {
+        fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=d4da6f83d8fa5dad990cafe88cb4fbf7')
+            .then( response => response.json() )
+            .then( data => this.setState({arrayPelicula:data.results}))/* chequear el results de aca con la info que me trae la api*/
+            .catch( error => console.log('El error fue: ' + error))
+    }
      /* hacer el fetch aca*/
     render(){
         return(
             <>
+                <section className='cardContainer'>
+                    {
+                        this.state.arrayPelicula.length > 0 ? (this.state.arrayPelicula.map((pelicula,idx)=> (<PeliPopularCard pelicula={pelicula} key={idx}/>))) : (<p>Cargando...</p>)   
+                    
+                    }
+                </section>
             </>
         )
     }
