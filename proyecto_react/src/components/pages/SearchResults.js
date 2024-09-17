030493ef152aa8358964854dad41f1279b43cf0b
jazbang <jbang@udesa.edu.ar>
2024-09-17 09:46:34 -0300

    loader en búsqueda. Faltcorregir algo para que me termine de funcionar resultados de busqueda

--- proyecto_react/src/components/pages/SearchResults.js
+++ proyecto_react/src/components/pages/SearchResults.js
@@ -1,25 +1,36 @@
 import { Component } from "react";
+import PeliculasGrid from "../PeliculasGrid/PeliculasGrid";
 
 
 class SearchResults extends Component{
     constructor(props){
         super(props);
         this.state={
-            movies:[],
+            arrayPelicula:[],
+            isLoding: true
         }
     }
 
     componentDidMount( ) {
+        this.setState({
+            isLoading: true
+        })
+ 
         fetch(`https://api.themoviedb.org/3/search/movie?query=${this.props.location.state.query}&api_key=d4da6f83d8fa5dad990cafe88cb4fbf7`)
             .then( (response) => response.json() )
-            .then( (data) => this.setState({movies:data.results}))
+            .then( (data) => this.setState({arrayPelicula:data.results, isLoading:false}))
             .catch( error => console.log('El error fue: ' + error))
     }
 
     render(){
         return(
             <>
-                <p>{this.props.location.state.query}</p>
+                {this.state.isLoding ? (
+                    <PeliculasGrid arrayPelicula={this.state.arrayPelicula}/>
+                ) : (
+                    <p>Cargando...</p>
+                )
+            }
             </>
         )
     }
