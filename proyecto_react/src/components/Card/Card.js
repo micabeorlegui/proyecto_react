e608e5d564a49c6fc06f2ee414e36a3491b59744
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-09-18 17:02:45 -0300

    agregue el boton de favoritos y modifique el css

--- proyecto_react/src/components/Card/Card.js
+++ proyecto_react/src/components/Card/Card.js
@@ -7,7 +7,9 @@ class Card extends Component{
     constructor(props){
         super(props);
         this.state={
-            verDescripcion: false
+            verDescripcion: false,
+            esFavorito: false,
+            movies: [] 
 
         }
     }
@@ -16,20 +18,26 @@ class Card extends Component{
             verDescripcion: !this.state.verDescripcion
         })
     }
+    
+
     render(){
         const {title,overview, poster_path}= this.props.pelicula
         return(
             <>
                 <article className='character-card'>
                         <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={title} />
-                        <h2> {title}</h2> {/* Nombre */} 
+                        <h2> {title}</h2> 
                         <p className='more' onClick={()=> this.verDescripcion()}>{this.state.verDescripcion ? "Ver menos" : 'Ver descripción'}</p> 
                         {this.state.verDescripcion &&(                          
                             <section className='extra'>                 
                             <p>Descripción: {overview} </p>               
                             </section>                              
                         )}
-                        <Link to="/detalle">Ir a detalle</Link>
+                        <div className="button-container">
+                            <Link to="/detalle">Ir a detalle</Link>
+                            <button>Agregar a favoritos</button>
+                        </div>
+                        
                     </article>
             </>
         )
