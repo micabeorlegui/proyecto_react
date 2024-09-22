77862840ef5010cd2fd5a9a81a4cecafd0bdc534
jazbang <jbang@udesa.edu.ar>
2024-09-22 18:11:57 -0300

    agregué condición en búsqueda para cuando no hayan resultados de búsqueda

--- proyecto_react/src/components/pages/SearchResults.js
+++ proyecto_react/src/components/pages/SearchResults.js
@@ -26,14 +26,20 @@ class SearchResults extends Component{
     render(){
         return(
             <>
-                <h2>Resultados de búsqueda: {this.props.location.state.query}</h2>
-                
-                {this.state.isLoding ? (
-                    <PeliculasGrid arrayPelicula={this.state.arrayPelicula} url={`https://api.themoviedb.org/3/search/movie?query=${this.props.location.state.query}&api_key=d4da6f83d8fa5dad990cafe88cb4fbf7`}/>
+                {this.state.arrayPelicula.length>0 ? (
+                    <>
+                        <h2>Resultados de búsqueda: {this.props.location.state.query}</h2>
+                        {this.state.isLoding ? (
+                            <PeliculasGrid arrayPelicula={this.state.arrayPelicula} url={`https://api.themoviedb.org/3/search/movie?query=${this.props.location.state.query}&api_key=d4da6f83d8fa5dad990cafe88cb4fbf7`}/>
+                        ) : (
+                            <p>Cargando...</p>
+                        )
+                        }
+                    </>
+
                 ) : (
-                    <p>Cargando...</p>
-                )
-                }
+                
+                    <h2>No hay resultados de búsqueda para {this.props.location.state.query} :/</h2>)}
 
             </>
         )
