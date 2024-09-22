9ddb9073a980c30d1b33e706365d2a826e9aec56
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-09-22 11:55:35 -0300

    agregue el link de Ver todas en las secciones del Home

--- proyecto_react/src/components/pages/Home.js
+++ proyecto_react/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormularioSearch from '../FormularioSearch/FormularioSearch';
 import PeliculasGrid from '../PeliculasGrid/PeliculasGrid';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 class Home extends Component{
     constructor(props){
@@ -18,8 +19,10 @@ class Home extends Component{
                     <FormularioSearch history={this.props.history}/>
                     <h2>Películas más populares</h2>
                     <PeliculasGrid url={'https://api.themoviedb.org/3/movie/top_rated?api_key=d4da6f83d8fa5dad990cafe88cb4fbf7'}/>
+                    <Link to="/populares" className='Link'>Ver todas</Link>
                     <h2>Películas recomendadas</h2>
                     <PeliculasGrid url={'https://api.themoviedb.org/3/discover/movie?api_key=d4da6f83d8fa5dad990cafe88cb4fbf7'}/>
+                    <Link to="/recomendadas"className='Link'>Ver todas</Link>
                 </main>
             </>
         )
