3f6a249887dadd6183a94ab1ba258350685515c5
tomicaast <castrot@udesa.edu.ar>
2024-09-29 20:15:16 -0300

    Pagina de Favoritos terminada, y CSS adaptado para que quede prolija

--- proyecto_react/src/components/pages/Favoritos.js
+++ proyecto_react/src/components/pages/Favoritos.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Card from '../Card/Card';
+import { Component } from "react";
+import '../pages/css/Home.css';
 
-const Favoritos = (props) => {
-    return (
-        <>
+class Favoritos extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            movies: []
+        }
+    }
+    componentDidMount(){
+        const storage= localStorage.getItem('favorites')
+        if(storage !== null){
+            const parsedStorage= JSON.parse(storage)
+            Promise.all(
+                parsedStorage.map(id=> 
+                    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=d4da6f83d8fa5dad990cafe88cb4fbf7`)
+                    .then(response => response.json())
+            ))
+            .then((data)=>{this.setState({movies: data})
+        })
+            
+        }
+    }
+    render(){
+        return(
+              <div>
+                <h1 className="title" >Favoritos</h1>
+                {this.state.movies.length=== 0 ?(<p>No hay películas en favoritos.</p>) :
+                ( <section className="favoritos">
+                    {this.state.movies.map(peliculas => (
+                          <Card id={peliculas.id} pelicula={peliculas} />
+                  ))}
+                    </section>)
+                  }
+            </div>
+           
+        )
+    }
+}
 
-        </>
-    );
-};
-
-export default Favoritos;
+export default Favoritos
